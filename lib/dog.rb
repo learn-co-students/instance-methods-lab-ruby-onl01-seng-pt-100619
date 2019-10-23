7f36b96e2d2ba4a21586edae705fb5e203e5f5e1
Joseph Miller <jdworldtv@gmail.com>
2019-10-23 17:56:18 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+
+
+class Dog do
+  
+   def bark  
+    puts "woof!"
+    
+  end
+end
+
+fido.Dog.new
+fido.bark #> "woof!"
+
+class Dog do
+
+def sit 
+  puts "The Dog is sitting"
+
+ end
+end
+
+fido.Dog.new
+fido.sit #> "The Dog is sitting!"
